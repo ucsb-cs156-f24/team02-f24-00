092e30abac1b0246c4ab04e047449dd89128ce9f
Daniel Jensen <djensen2@outlook.com>
2024-11-02 17:24:39 -0700

    dj - forgot the linter existed. ran it now

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,42 +1,44 @@
 const helpRequestFixtures = {
-    oneHelpRequest:[
-        {
-            id: 1,
-            requesterEmail: "djensen@ucsb.edu",
-            teamId: "staff",
-            tableOrBreakoutRoom: "table 17",
-            requestTime: "2024-03-11T08:00:00",
-            explanation: "Need clarification on a PR",
-            solved: "true"
-        }
-    ],
-    threeHelpRequest:[
-        {
-            id: 1,
-            requesterEmail: "djensen@ucsb.edu",
-            teamId: "staff",
-            tableOrBreakoutRoom: "table 17",
-            requestTime: "2024-03-11T08:00:00",
-            explanation: "Need clarification on a PR",
-            solved: "true"
-        },
-        {
-            id: 2,
-            requesterEmail: "djensen2@outlook.com",
-            teamId: "5pm-4",
-            tableOrBreakoutRoom: "table 12",
-            requestTime: "2024-10-11T08:00:00",
-            explanation: "Need staff approval for a retro",
-            solved: "false"
-        },
-        {
-            id: 3,
-            requesterEmail: "phtcon@ucsb.edu",
-            teamId: "5",
-            tableOrBreakoutRoom: "table 5",
-            requestTime: "2024-12-23T08:00:00",
-            explanation: "Need code support",
-            solved: "true"
-        }
-    ]
-}
+  oneHelpRequest: [
+    {
+      id: 1,
+      requesterEmail: "djensen@ucsb.edu",
+      teamId: "staff",
+      tableOrBreakoutRoom: "table 17",
+      requestTime: "2024-03-11T08:00:00",
+      explanation: "Need clarification on a PR",
+      solved: "true",
+    },
+  ],
+  threeHelpRequest: [
+    {
+      id: 1,
+      requesterEmail: "djensen@ucsb.edu",
+      teamId: "staff",
+      tableOrBreakoutRoom: "table 17",
+      requestTime: "2024-03-11T08:00:00",
+      explanation: "Need clarification on a PR",
+      solved: "true",
+    },
+    {
+      id: 2,
+      requesterEmail: "djensen2@outlook.com",
+      teamId: "5pm-4",
+      tableOrBreakoutRoom: "table 12",
+      requestTime: "2024-10-11T08:00:00",
+      explanation: "Need staff approval for a retro",
+      solved: "false",
+    },
+    {
+      id: 3,
+      requesterEmail: "phtcon@ucsb.edu",
+      teamId: "5",
+      tableOrBreakoutRoom: "table 5",
+      requestTime: "2024-12-23T08:00:00",
+      explanation: "Need code support",
+      solved: "true",
+    },
+  ],
+};
+
+export {helpRequestFixtures}
