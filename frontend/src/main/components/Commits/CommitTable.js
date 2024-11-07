bf6ae616ddf1caf61c1d20f0ebec6c19e4852f4c
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-11-06 20:50:21 -0800

    pc - add CommitTable, tests and stories

---
+++ frontend/src/main/components/Commits/CommitTable.js
@@ -0,0 +1,71 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+    cellToAxiosParamsDelete,
+    onDeleteSuccess,
+} from "main/utils/commitUtils";
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function CommitTable({
+    commits,
+    currentUser,
+    testIdPrefix = "CommitTable",
+}) {
+    const navigate = useNavigate();
+
+    const editCallback = (cell) => {
+        navigate(`/commits/edit/${cell.row.values.id}`);
+    };
+
+    // Stryker disable all : hard to test for query caching
+
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/commits/all"],
+    );
+    // Stryker restore all
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => {
+        deleteMutation.mutate(cell);
+    };
+
+    const columns = [
+        {
+            Header: "id",
+            accessor: "id", // accessor is the "key" in the data
+        },
+
+        {
+            Header: "Message",
+            accessor: "message",
+        },
+        {
+            Header: "URL",
+            accessor: "url",
+        },
+        {
+            Header: "Commit Time (in UTC)",
+            accessor: "commitTime",
+        },
+        {
+            Header: "Author Login",
+            accessor: "authorLogin",
+        },
+    ];
+
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        columns.push(ButtonColumn("Edit", "primary", editCallback, testIdPrefix));
+        columns.push(
+            ButtonColumn("Delete", "danger", deleteCallback, testIdPrefix),
+        );
+    }
+
+    return (
+        <OurTable data={commits} columns={columns} testid={testIdPrefix} />
+    );
+}
