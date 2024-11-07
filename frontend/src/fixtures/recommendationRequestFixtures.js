af40ecbf61326c32724c620eaac48f38eab5b36f
Jenil Prajapati <jenilrajeshkumar@ucsb.edu>
2024-11-06 22:24:42 -0800

    jp - minor error in fixtures

--- frontend/src/fixtures/recommendationRequestFixtures.js
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -1,42 +1,50 @@
 const recommendationRequestFixtures = {
-    oneRecommendationRequest: {
-      id: 1,
-      requesterEmail: "anvithakosuri@gmail.com",
-      professorEmail: "dimirza@cs.ucsb.edu",
-      explanation: "I would like a recommendation for UCSB BS/MS program",
-      dateRequested: "2022-04-03T12:00:00",
-      dateNeeded: "2022-06-04T12:00:00",
-      done: false,
-    },
-    threeRecommendationRequests: [
+  onerecommendationRequest:
+  [
+    {
+     "id": 1,
+      "requesterEmail": "requester email 1",
+      "professorEmail": "professor email 1",
+      "explanation": "explanation 1",
+      "dateRequested": "2022-01-02T12:00:00",
+      "dateNeeded": "2022-02-02T12:00:00",
+      "done": true  
+    }
+  ],
+
+  threerecommendationRequest:
+  [
       {
-        id: 1,
-        requesterEmail: "anvithakosuri@gmail.com",
-        professorEmail: "dimirza@cs.ucsb.edu",
-        explanation: "I would like a recommendation for UCSB BS/MS program",
-        dateRequested: "2022-04-03T12:00:00",
-        dateNeeded: "2022-06-04T12:00:00",
-        done: false,
+          "id": 2,
+           "requesterEmail": "requester email 2",
+           "professorEmail": "professor email 2",
+           "explanation": "explanation 2",
+           "dateRequested": "2022-03-02T12:00:00",
+           "dateNeeded": "2022-04-02T12:00:00",
+           "done": true  
       },
+
       {
-        id: 2,
-        requesterEmail: "anvithakosuri@gmail.com",
-        professorEmail: "ziad.matni@ucsb.edu",
-        explanation: "I would like a recommendation for a scholarship",
-        dateRequested: "2022-07-24T12:00:00",
-        dateNeeded: "2022-09-04T12:00:00",
-        done: true,
+          "id": 3,
+           "requesterEmail": "requester email 3",
+           "professorEmail": "professor email 3",
+           "explanation": "explanation 3",
+           "dateRequested": "2022-05-02T12:00:00",
+           "dateNeeded": "2022-06-02T12:00:00",
+           "done": false  
       },
+
       {
-        id: 3,
-        requesterEmail: "anvithakosuri@gmail.com",
-        professorEmail: "pconrad@cs.ucsb.edu",
-        explanation: "I would like a recommendation for PhD at Stanford",
-        dateRequested: "2022-12-12T12:00:00",
-        dateNeeded: "2023-01-01T12:00:00",
-        done: false,
-      },
-    ],
-  };
-  
-  export { recommendationRequestFixtures };
+          "id": 4,
+           "requesterEmail": "requester email 4",
+           "professorEmail": "professor email 4",
+           "explanation": "explanation 4",
+           "dateRequested": "2022-07-02T12:00:00",
+           "dateNeeded": "2022-08-02T12:00:00",
+           "done": false  
+      }
+
+  ]
+};
+
+export { recommendationRequestFixtures };
